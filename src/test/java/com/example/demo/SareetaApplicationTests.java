10b63bc0174bcaa01d31f57f6d3dedd8d0d8c6a7
Yutian <yutiancoding@gmail.com>
2021-02-24 13:02:39 -0500

    add UserControllerTest

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.tests.UserControllerTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ UserControllerTest.class })
 @SpringBootTest
 public class SareetaApplicationTests {
 
