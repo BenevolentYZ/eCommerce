4a0b2975b47999d4bb87db1356be7589a4eb43af
Yutian <yutiancoding@gmail.com>
2021-02-24 13:04:57 -0500

    add OrderControllerTest

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.tests.OrderControllerTest;
 import com.example.demo.tests.UserControllerTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ UserControllerTest.class })
+@Suite.SuiteClasses({ UserControllerTest.class, OrderControllerTest.class })
 @SpringBootTest
 public class SareetaApplicationTests {
 
